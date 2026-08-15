cb0d7351997626920ba49ca754773d0827295fd0
parthibhan20 <parthibhanbmnp@gmail.com>
2026-08-15 22:17:58 +0530

    package 3

---
+++ pkg3/ccc.java
@@ -0,0 +1,16 @@
+package pkg3;
+import java.util.*;
+public class ccc
+{
+    int c,d;
+    
+    public void pottuvidu()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number:");
+        c=sc.nextInt();
+        d=sc.nextInt();
+        System.out.println("multiplication:"+(c*d));
+    }
+
+}
