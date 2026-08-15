458000183f316583a372a21fa26708029114bd66
parthibhan20 <parthibhanbmnp@gmail.com>
2026-08-15 22:06:43 +0530

    package 1

---
+++ pkg1/Aaa.java
@@ -0,0 +1,13 @@
+package pkg1;
+public class Aaa
+{
+   public void fndisp()
+    {
+        System.out.println("I am parthibhan");
+
+    }
+    public void fnshow()
+    {
+        System.out.println("This is package");
+    }
+}
