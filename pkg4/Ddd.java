93e818418d83584b45a626edf7c18a5e02500f26
parthibhan20 <parthibhanbmnp@gmail.com>
2026-08-15 22:33:52 +0530

    package 4

--- pkg4/Ddd.java
+++ pkg4/Ddd.java
@@ -1,1 +1,22 @@
-
+package pkg4;
+import java.util.*;
+public class Ddd
+{
+     int e,f;
+     
+     public void fndivi()
+     {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the elements:");
+        e=sc.nextInt();
+        f=sc.nextInt();
+        if(f==0)
+        {
+            System.out.println("division error");            
+        }
+        else
+        {
+            System.out.println("division is:"+(e/f));
+        }    
+    }
+}
