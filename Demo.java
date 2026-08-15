f26dad91d7873536734915e6b3c908cd5bb32fcc
parthibhan20 <parthibhanbmnp@gmail.com>
2026-08-15 22:37:01 +0530

    Importing those packages(1,2,3,4) in main demo class

---
+++ Demo.java
@@ -0,0 +1,19 @@
+import pkg1.*;
+import pkg2.Bbb;
+import pkg3.ccc;
+import pkg4.*;
+ class Demo
+{
+    public static void main(String as[])
+    {
+        Aaa o1=new Aaa();
+        Bbb o2=new Bbb();
+        ccc o3=new ccc();
+        Ddd o4=new Ddd();
+        o1.fndisp();
+        o1.fnshow();
+        o2.fnadd();
+        o3.pottuvidu();
+        o4.fndivi();
+    }
+}
